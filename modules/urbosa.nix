701e452d4b87b5ff0537022ea1ab893e893f9220
Churchill Lee <hello@feliciousx.dev>
2022-09-10 12:06:15 +1000

    tmp

--- modules/urbosa.nix
+++ modules/urbosa.nix
@@ -48,6 +48,49 @@
   networking.useDHCP = false;
   networking.interfaces.wlp59s0.useDHCP = true;
 
+  # zenu related
+  # TODO: check if work?
+  networking.extraHosts = 
+    ''
+      127.0.0.1 kibana.local
+      127.0.0.1 zenu.dv
+      127.0.0.1 www.zenu.dv
+      127.0.0.1 zone.zenu.dv
+      127.0.0.1 db.zenu.dv
+      127.0.0.1 es.zenu.dv
+      127.0.0.1 memcached.zenu.dv
+      127.0.0.1 redis.zenu.dv
+      127.0.0.1 tunnel.zenu.dv
+      127.0.0.1 s3.zenu.dv
+      127.0.0.1 zenu.test
+      127.0.0.1 www.zenu.test
+      127.0.0.1 subzero.dv
+      127.0.0.1 www.subzero.dv
+      127.0.0.1 subzero.test
+      127.0.0.1 www.subzero.test
+      127.0.0.1 dev.zenu.com.au
+      127.0.0.1 local-zenu.ap-southeast-2.es.amazonaws.com
+      127.0.0.1 db.westeros.dv
+      127.0.0.1 es.braavos.dv
+      127.0.0.1 es.westeros.dv
+      127.0.0.1 memcached.westeros.dv
+      127.0.0.1 westeros.dv
+      127.0.0.1 www.westeros.dv
+      127.0.0.1 www.westeros.test
+      127.0.0.1 admin.westeros.dv
+      127.0.0.1 braavos.dv
+      127.0.0.1 www.braavos.dv
+      127.0.0.1 www.braavos.test
+      127.0.0.1 business.braavos.dv
+      127.0.0.1 www.business.braavos.dv
+      127.0.0.1 holiday.braavos.dv
+      127.0.0.1 www.holiday.braavos.dv
+      127.0.0.1 rural.braavos.dv
+      127.0.0.1 www.rural.braavos.dv
+      127.0.0.1 www.bytey.dv
+      127.0.0.1 dev.developerrealestateview.com.au
+    '';
+
   # urbosa-only programs
   environment.systemPackages = with pkgs; [
     # browsers
