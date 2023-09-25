1794d8561d5442967b4dff49d41390f7b18263ce
Churchill Lee <hello@feliciousx.dev>
2023-09-25 23:44:22 +1000

    feat: beef up my setup with more apps

--- hosts/home.nix
+++ hosts/home.nix
@@ -7,15 +7,17 @@
 
     packages = with pkgs; [
       # Terminal
-      btop              # Resource Manager
       bat
-      direnv
       bottom
+      btop              # Resource Manager
+      delta
+      direnv
       du-dust
       exa
       fd
       fzf
       fzy
+      gping
       gum
       hyperfine
       jq
@@ -28,6 +30,7 @@
       tmuxp
 
       # Video/Audio
+      exiftool
       vlc               # Media Player
       ffmpeg-full
 
@@ -43,6 +46,7 @@
       #pcmanfm           # File Manager
       #p7zip             # Zip Encryption
       rsync             # Syncer - $ rsync -r dir1/ dir2/
+      syncthing         # File Sync
       unzip             # Zip Files
       #unrar             # Rar Files
       zip               # Zip
@@ -56,26 +60,26 @@
       #usbutils         # USB Utility Info
       #wacomtablet      # Wacom Tablet
       wget             # Downloader
+      ipfs              # protocol
       #zsh              # Shell
       #
       # General home-manager
-      alacritty        # Terminal Emulator
-      dunst            # Notifications
+      alacritty         # Terminal Emulator
+      dunst             # Notifications
       #doom emacs       # Text Editor
-      libnotify        # Dependency for Dunst
+      libnotify         # Dependency for Dunst
       #neovim           # Text Editor
       #rofi             # Menu
       #rofi-power-menu  # Power Menu
       #udiskie          # Auto Mounting
-      neovim              # Text Editor
-      helix
-      obsidian
+      neovim            # Text Editor
+      helix             # Text Editor
       yubikey-agent
       yubikey-manager
       yubioath-flutter
       #
       # Xorg configuration
-      xclip            # Console Clipboard
+      xclip             # Console Clipboard
       #xorg.xev         # Input Viewer
       #xorg.xkill       # Kill Applications
       #xorg.xrandr      # Screen Settings
@@ -111,6 +115,7 @@
       deluge           # Torrents
       discord          # Chat
       #ffmpeg           # Video Support (dslr)
+      gimp
       #gmtp             # Mount MTP (GoPro)
       #gphoto2          # Digital Photography
       #handbrake        # Encoder
@@ -119,6 +124,8 @@
       #lutris           # Game Launcher
       #mkvtoolnix       # Matroska Tool
       #nvtop            # Videocard Top
+      obsidian          # journaling
+      orca              # Screen Reader
       #plex-media-player# Media Player
       #prismlauncher    # MC Launcher
       #steam            # Games
