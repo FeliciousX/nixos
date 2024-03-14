68898e8660655736b72e224686eb0210b13ae4cc
Churchill Lee <hello@feliciousx.dev>
2024-03-14 16:52:28 +1100

    fix(helix): invalid language server arguments

--- hosts/programs/helix.nix
+++ hosts/programs/helix.nix
@@ -5,7 +5,7 @@
 	languages = {
 			language-server.typescript-language-server = with pkgs.nodePackages; {
 				command = "${typescript-language-server}/bin/typescript-language-server";
-				args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib" ];
+				args = [ "--stdio" ];
 			};
 			language = [
 			{
