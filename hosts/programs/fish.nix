f1f411b9bced4c1daca47424b088a396102b1bc7
Churchill Lee <hello@feliciousx.dev>
2024-03-21 07:46:44 +1100

    feat(programs/fish): add shellAbbrs
    z for zellij

--- hosts/programs/fish.nix
+++ hosts/programs/fish.nix
@@ -1,5 +1,8 @@
 {
 	enable = true;
+	shellAbbrs = {
+		z = "zellij"; # TODO: check if zellij is enabled before setting abbr
+	};
 	functions = {
 		jira = "xdg-open https://zenusoftware.atlassian.net/browse/{$argv[1]}";
 	};
