9405fa48cfe94dcd9b5f56c1621359afe3c45dd8
Churchill Lee <hello@feliciousx.dev>
2024-06-12 08:42:31 +1000

    feat(git): move aliases from fish to git config

--- hosts/programs/fish/default.nix
+++ hosts/programs/fish/default.nix
@@ -1,28 +1,6 @@
 {
 	enable = true;
 	shellAbbrs = {
-		ga = "git add";
-		gb = "git branch";
-		gbD  = "git branch -D";
-		gc = "git commit -v";
-		gco = "git checkout";
-		gcp = "git cherry-pick";
-		gcpa = "git cherry-pick --abort";
-		gcpc = "git cherry-pick --continue";
-		gd = "git diff";
-		gds = "git diff --staged";
-		gf = "git fetch";
-		glgo = "git log --graph --oneline";
-		glog = "git log";
-		gm = "git merge";
-		grb = "git rebase";
-		grba = "git rebase --abort";
-		grbc = "git rebase --continue";
-		grbi = "git rebase --interactive";
-		grst = "git restore";
-		grsts = "git restore --staged";
-		gst = "git status";
-		gsw = "git switch";
 		z = "zellij"; # TODO: check if zellij is enabled before setting abbr
 		zenucompose = "docker compose -f docker-compose.yml -f docker-compose-zenu.yml";
 	};
