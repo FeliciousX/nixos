39b3bea873ef9e72292a54fe1ec700c698e4cf48
FeliciousX <hello@feliciousx.dev>
2024-06-13 23:49:46 +1000

    feat(fail2ban): ignore local network

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -79,6 +79,9 @@
       enable = true;
       maxretry = 1;
       bantime-increment.enable = true;
+      ignoreIP = [
+        "192.168.31.0/24" # TODO: divide my network subnets for homelab
+      ];
     };
 
     pipewire = {
