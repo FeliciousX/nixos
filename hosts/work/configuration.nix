811f3c9f952f2d9e2c09644053420814d5eb71b2
FeliciousX <hello@feliciousx.dev>
2024-06-16 02:11:40 +1000

    trying to fix shit

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -207,6 +207,7 @@
       keep-outputs          = true
       keep-derivations      = true
     '';
+    nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
   };
 
   # This value determines the NixOS release from which the default
@@ -215,5 +216,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
