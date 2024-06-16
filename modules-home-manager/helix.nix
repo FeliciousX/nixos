e41cba7227c2e276ba9483023c496e31972f79e3
FeliciousX <hello@feliciousx.dev>
2024-06-16 23:27:48 +1000

    feat(helix): add nixpkgs-fmt formatter on helix

--- modules-home-manager/helix.nix
+++ modules-home-manager/helix.nix
@@ -1,69 +1,79 @@
-{ pkgs }:
+{ pkgs, ... }:
 
 {
-	enable = true;
-	defaultEditor = true;
-	languages = {
-			language-server.typescript-language-server = with pkgs.nodePackages; {
-				command = "${typescript-language-server}/bin/typescript-language-server";
-				args = [ "--stdio" ];
-			};
-			language = [
-			{
-				name = "erb";
-				auto-format = true;
-				file-types = ["cfm"];
-				indent = {
-					tab-width = 2;
-					unit = "\t";
-				};
-			}
-			{
-				name = "java";
-				file-types = ["cfc"];
-				indent = {
-					tab-width = 2;
-					unit = "\t";
-				};
-			}
-			{
-				name = "typescript";
-				auto-format = true;
-			}
-			{
-				name = "tsx";
-				auto-format = true;
-			}
-		];
-	};
-	settings = {
-		theme = "dracula";
-		editor = {
-			mouse = false;
-			line-number = "relative";
-			bufferline = "multiple";
-			lsp.display-messages = true;
-		};
-		editor.cursor-shape = {
-			insert = "bar";
-			normal = "block";
-			select = "underline";
-		};
-		editor.whitespace = {
-			render = "all";
-			characters = { newline = "¬"; };
-		};
-		keys.insert = {
-			j = {
-				k = "normal_mode";
-			};
-		};
-		keys.normal = {
-			esc = ["collapse_selection" "keep_primary_selection"];
-			A-t = {
-				g = ":toggle-option file-picker.git-ignore";
-				h = ":toggle-option file-picker.hidden";
-			};
-		};
-	};
+  enable = true;
+  defaultEditor = true;
+  languages = {
+    language-server = {
+      typescript-language-server = with pkgs.nodePackages; {
+        command = "${typescript-language-server}/bin/typescript-language-server";
+        args = [ "--stdio" ];
+      };
+    };
+    language = [
+      {
+        name = "nix";
+        auto-format = true;
+        file-types = [ "nix" ];
+        formatter = {
+          command = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+        };
+      }
+      {
+        name = "erb";
+        auto-format = true;
+        file-types = [ "cfm" ];
+        indent = {
+          tab-width = 2;
+          unit = "\t";
+        };
+      }
+      {
+        name = "java";
+        file-types = [ "cfc" ];
+        indent = {
+          tab-width = 2;
+          unit = "\t";
+        };
+      }
+      {
+        name = "typescript";
+        auto-format = true;
+      }
+      {
+        name = "tsx";
+        auto-format = true;
+      }
+    ];
+  };
+  settings = {
+    theme = "dracula";
+
+    editor.mouse = true;
+    editor.line-number = "relative";
+    editor.bufferline = "multiple";
+    editor.soft-wrap.enable = true;
+    editor.lsp.display-messages = true;
+
+    editor.cursor-shape = {
+      insert = "bar";
+      normal = "block";
+      select = "underline";
+    };
+
+    editor.whitespace = {
+      render = "all";
+      characters = { newline = "¬"; };
+    };
+
+    # insert mode
+    keys.insert.j.k = "normal_mode";
+
+    # normal mode
+    keys.normal.esc = [ "collapse_selection" "keep_primary_selection" ];
+    keys.normal.A-t.g = ":toggle-option file-picker.git-ignore";
+    keys.normal.A-t.h = ":toggle-option file-picker.hidden";
+
+
+  };
 }
