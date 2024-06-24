d5118b63a880679661ca53da0674029d146de468
FeliciousX <hello@feliciousx.dev>
2024-06-24 22:24:35 +1000

    feat(lf): add keybindings and commands

--- modules-home-manager/lf.nix
+++ modules-home-manager/lf.nix
@@ -1,3 +1,30 @@
 {
-  programs.lf.enable = true;
+  programs.lf = {
+    enable = true;
+    keybindings = {
+      J = "move-parent down";
+      K = "move-parent up";
+      p = ":paste; clear";
+      f = "$hx $(fzf)";
+      "<c-f>" = ":fzf_jump";
+    };
+    commands = {
+      move-parent = ''''${{
+        dironly="setlocal '$(dirname "$PWD")' dironly"
+        lf -remote "send $id :updir; $dironly true; $1; $dironly false; open"
+      }}'';
+      fzf_jump = ''''${{
+        res="$(find . -maxdepth 1 | fzf --reverse --header='Jump to location')"
+            if [ -n "$res" ]; then
+                if [ -d "$res" ]; then
+                    cmd="cd"
+                else
+                    cmd="select"
+                fi
+                res="$(printf '%s' "$res" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+                lf -remote "send $id $cmd \"$res\""
+            fi
+      }}'';
+    };
+  };
 }
