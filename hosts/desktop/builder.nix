4776fc2533bb30c58271e154653a38be63644953
FeliciousX <hello@feliciousx.dev>
2024-06-30 23:58:09 +1000

    feat(builder): setup builder

---
+++ hosts/desktop/builder.nix
@@ -0,0 +1,10 @@
+{ config, lib, user, ... }:
+
+{
+  users.users.nixremote = {
+    openssh.authorizedKeys.keys =
+      [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJao7dzj4D0cG7W3mehI5slhFo/fMyU7cpNr9HAhRAXB root@tabantha"
+      ];
+  };
+}
--- hosts/work/distributed-build.nix
+++ hosts/work/distributed-build.nix
@@ -4,11 +4,19 @@
   nix.distributedBuilds = true;
   nix.buildMachines = [
     {
-      hostName = "desktop";
+      hostName = "hyrule.local";
       systems = [ "x86_64-linux" ];
       maxJobs = 4;
       speedFactor = 2;
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
     }
   ];
+
+  programs.ssh.extraConfig = ''
+    Host hyrule.local
+      HostName hyrule.local
+      User nixremote
+      IdentitiesOnly yes
+      IdentityFile /root/.ssh/nixremote
+  '';
 }
