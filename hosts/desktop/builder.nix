a4062512014c26dbfb35e5e91b48808e37349447
FeliciousX <hello@feliciousx.dev>
2024-07-01 00:06:49 +1000

    fix(nixremote): set normalUser flag

--- hosts/desktop/builder.nix
+++ hosts/desktop/builder.nix
@@ -2,6 +2,8 @@
 
 {
   users.users.nixremote = {
+    isNormalUser = true;
+
     openssh.authorizedKeys.keys =
       [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJao7dzj4D0cG7W3mehI5slhFo/fMyU7cpNr9HAhRAXB root@tabantha"
