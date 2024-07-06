d5b627b3ed30d7c57da4cf42dae11c4be3b8c8e9
FeliciousX <hello@feliciousx.dev>
2024-07-07 01:24:34 +1000

    feat(kokiri): allow laptop to ssh into kokiri

--- hosts/kokiri/ssh.nix
+++ hosts/kokiri/ssh.nix
@@ -10,6 +10,7 @@
   users.users.nixos = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPG8dpG+gfvMJCjw92U9uewjaZA+0QGeRi/ir19RFDK9 fx@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzBvauAQglStcoos5RaFC6ITuOavBYksmuNtbOW2R+o xps15@feliciousx.com"
     ];
   };
 }
